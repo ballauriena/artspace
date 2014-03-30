6e8c988ed38b58df23a53d83b6947d756ccf04b6
Laura <lleecrowe@gmail.com>
2014-03-30 10:21:04 -0500

    updated photos model so attachment has different sizes

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -1,3 +1,13 @@
 class Photo < ActiveRecord::Base
   belongs_to :space
+
+  # This method associates the attribute ":uploaded_photo" with a file attachment
+  has_attached_file :uploaded_photo, styles: {
+    thumb: '100x100>',
+    square: '200x200#',
+    medium: '300x300>'
+  }
+
+  # Validate the attached image is image/jpg, image/png, etc
+  validates_attachment_content_type :uploaded_photo, :content_type => /\Aimage\/.*\Z/
 end
