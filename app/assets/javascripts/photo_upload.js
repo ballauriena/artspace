aa527716085756f815fe63f83e2538c086d49558
Laura <lleecrowe@gmail.com>
2014-04-05 15:22:00 -0500

    updated javascript to delete photo fields dynamically

--- app/assets/javascripts/photo_upload.js
+++ app/assets/javascripts/photo_upload.js
@@ -1,4 +1,4 @@
-// function remove_photos (link) {
-// 	$(link).previous("input[type=hidden").value = "1";
-// 	$(link).up(".photos").hide();
-// }
+function remove_photos(link) {
+	$(link).prev("input[type=hidden").value = "1";
+	$(link).closest(".photo_fields").hide();
+}
