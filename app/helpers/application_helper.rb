62a06e2285d4214245b82997d2b8141d219e7d7a
Laura <lleecrowe@gmail.com>
2014-04-05 15:50:35 -0500

    pulled photo fields into its own partial

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+
+	def link_to_add_photo_fields(name, f)
+		new_photo = Photo.new
+		fields = f.fields_for(:photos, new_photo, :child_index => "new_photo") do |builder|
+			render("photo_fields", :f => builder)
+		end
+	end
+
 end
