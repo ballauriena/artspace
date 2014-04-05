b41b1ca1420b2c63d7e2b629556fc72754222985
Laura <lleecrowe@gmail.com>
2014-04-05 15:28:45 -0500

    added if statement so there must be one photo upload field on the page

--- app/assets/javascripts/photo_upload.js
+++ app/assets/javascripts/photo_upload.js
@@ -1,4 +1,6 @@
 function remove_photos(link) {
+	if($('.remove_photo_link').length > 1){
 	$(link).prev("input[type=hidden").value = "1";
-	$(link).closest(".photo_fields").hide();
+	$(link).closest(".photo_fields").remove();
+  }
 }
